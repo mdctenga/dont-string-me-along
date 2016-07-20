9231af18e0d84bd69850942379af53f9a0a2a0f2
mdctenga <mdctenga@gmail.com>
2016-07-20 08:53:53 -1000

    Write out initial sudo code

---
+++ index.js
@@ -0,0 +1,8 @@
+function findMissingNumber(argument) {
+  // break argument into an argument array
+  // iterate over the argument array
+  // with a test array of 0 - 9, check against the argument array
+    // if number exists in array, removed from test array
+  // Ideally, in the end you'll end up with an array of numbers that you didn't find.
+}
+findMissingNumber(9899100101103104105);
